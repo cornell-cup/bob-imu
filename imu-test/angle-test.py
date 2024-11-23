b9253674bcd9c87a2430c12b0421bb96b68ec488
Andrew Lin <ahl92@cornell.edu>
2024-11-23 14:28:52 -0500

    added two functions for complementary and kalman filters

--- imu-test/angle-test.py
+++ imu-test/angle-test.py
@@ -8,35 +8,36 @@ DT = 0.01
 
 imu = ICM20948()
 
-ALPHA = 0.98
+ALPHA = 0.9
 
-#Calculate angle based on imu accelerometer and gyroscope data
-def calculate_angle(accel_x, accel_y, accel_z,gyro_x, gyro_y, gyro_z,roll):
+# Complementary Filter
+# Calculate angle based on imu accelerometer and gyroscope data
+def calculate_angle_comp(accel_x, accel_y, accel_z,gyro_x, gyro_y, gyro_z,angle):
   # Calculate the angle from the accelerometer
   accel_roll = math.atan2(accel_y, accel_z) * 180 / math.pi
   # Integrate the gyroscope data
-  roll += gyro_x * DT
+  gyro_roll = angle + gyro_x * DT
   # Apply the complementary filter
-  roll = ALPHA * roll + (1 - ALPHA) * accel_roll
-  return roll
+  angle = ALPHA * gyro_roll + (1 - ALPHA) * accel_roll
+  return angle
 
-# Convert degrees to Dynamixel position
-def angle_to_position(angle):
-  return int(((-angle+90) / 360.0) * 4095)
-
+# Kalman Filter angle calculation
+def calculate_angle_kalman():
+  return 0
 
 def main():
     try:
         curr_angle = 0
         while True:
           start=time.clock_gettime(0)
-          accel_x, accel_y, accel_z,gyro_x, gyro_y, gyro_z = imu.read_accelerometer_gyro_data()
-          
+          accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z = imu.read_accelerometer_gyro_data()
           curr_angle = calculate_angle(accel_x, accel_y, accel_z,gyro_x, gyro_y, gyro_z,curr_angle)
 
           abs_g = math.sqrt(accel_x*accel_x+accel_y*accel_y+accel_z*accel_z)
-          if accel_z>0.995 and accel_z<1.005 and abs_g>0.95 and abs_g<1.05:
-              curr_angle = 0
+          
+          # Reset Logic
+          # if accel_z>0.995 and accel_z<1.005 and abs_g>0.95 and abs_g<1.05:
+          #     curr_angle = 0
 
           # Print current angle
           print(curr_angle)
