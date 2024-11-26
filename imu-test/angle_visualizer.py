87b2b84ea3438e5156f7bac8ed0eac2873d6fb99
Andrew Lin <ahl92@cornell.edu>
2024-11-26 14:34:55 -0500

    new file names

---
+++ imu-test/angle_visualizer.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+angle_test = "angle-test"
+import angle_test
+
+# Data for the graph
+x = time
+y1 = gyro_roll
+y2 = accel_roll
+y3 = complementary_roll
+
+# Create the plot
+plt.plot(x, y1, label='gyro_roll')
+plt.plot(x, y2, label='accel_roll')
+plt.plot(x, y3, label='complementary_roll')
+
+# Add labels and a legend
+plt.title('Example Graph')
+plt.xlabel('time')
+plt.ylabel('angle')
+plt.legend()
+
+# Show the plot
+plt.show()
+
+plt.savefig('example_graph.png')
+
+if __name__ == "__main__":
+    main()
