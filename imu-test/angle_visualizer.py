c6bd536a302d4a2ebbfbb9dd11c639eba959be80
Andrew Lin <smartmonkey3042@gmail.com>
2025-05-07 23:06:25 -0400

    implement kalman filter data

--- imu-test/angle_visualizer.py
+++ imu-test/angle_visualizer.py
@@ -5,7 +5,7 @@ import math
 import matplotlib.pyplot as plt
 
 import angle_test
-
+import angle_test_Rhea as kft
 DT = 0.01
 ALPHA = 0.9
 imu = ICM20948()
@@ -29,13 +29,26 @@ def main():
             loop_start = time.time()
 
             accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z = imu.read_accelerometer_gyro_data()
+            # Complementary filter
             accel_roll, gyro_roll, curr_angle = angle_test.calculate_angle_comp(accel_x, accel_y, accel_z,gyro_x, gyro_y, gyro_z,curr_angle)
             
+            # Kalman filter
+            z = kft.sensor_reading(accel_x, accel_y, accel_z, gyro_x, gyro_y, curr_angle)
+
+            # Predict and update steps for Kalman Filter
+            kft.kf.predict()
+            kft.kf.update(z)
+
+            # Extract filtered pitch and roll
+            # filtered_pitch = kft.kf.x[0][0]
+            filtered_roll = kft.kf.x[2][0]
+
             # Log data
             time_log.append(time.time() - start_time)
             accel_roll_log.append(accel_roll)
             gyro_roll_log.append(gyro_roll)
             comp_roll_log.append(curr_angle)
+            kalman_roll_log.append(filtered_roll)
 
             # Delay
             end = time.clock_gettime(0)
@@ -51,7 +64,7 @@ def main():
     r1 = accel_roll_log
     r2 = gyro_roll_log
     r3 = comp_roll_log
-    # r4 = kalman_roll
+    r4 = kalman_roll_log
     # p1 = accel_pitch
     # p2 = gyro_pitch
     # p3 = comp_pitch
@@ -61,7 +74,7 @@ def main():
     plt.plot(x, r1, label='gyro_roll')
     plt.plot(x, r2, label='accel_roll')
     plt.plot(x, r3, label='complementary_roll')
-    # plt.plot(x, r4, label='kalman_roll')
+    plt.plot(x, r4, label='kalman_roll')
 
     # Add labels and a legend
     plt.title('Roll Angle Over Time')
